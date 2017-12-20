be2656e3977daf02a6c6fdaf64a6bc0a35218b65
Nick <nick@Nicks-MacBook-Pro.local>
2017-12-20 16:07:27 -0600

    Initial Commit

---
+++ ImageApp/DataModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  ImageApp
+//
+//  Created by Nick on 12/20/17.
+//  Copyright © 2017 Nick. All rights reserved.
+//
+
+import UIKit
+
+public class DataModel{
+    
+    public var aTestString = ""
+    
+}
--- ImageApp/ViewController.swift
+++ ImageApp/ViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIScrollViewDelegate{
+
+   
+    
+    @IBOutlet weak var aUiView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imgPhoto: UIImageView!
+    let DM = DataModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
+        self.DM.aTestString = "this"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +31,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        
+        return aUiView
+    }
 
 }
 
