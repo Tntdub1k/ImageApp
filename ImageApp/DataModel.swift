777bb52c8f9c58c79c69caa7754847fc8365f760
Nick <nick@Nicks-MacBook-Pro.local>
2017-12-21 10:27:43 -0600

    Added in ADB model

--- ImageApp/DataModel.swift
+++ ImageApp/DataModel.swift
@@ -87,7 +87,7 @@ public class RotateableRing{
                 if (success == true){
                     break
                 }
-                if (i == newR.count -1){
+                if (i == newR.count - 1){
                     //Error, maxed out rings at newR[i].CurrentCelestialBody
                     success = true
                 }
@@ -96,5 +96,209 @@ public class RotateableRing{
         return newR
     }
     
+    func AdvanceTo(advancement:Int, inputRing:Array<RingEntry>) -> Array<RingEntry>{
+        
+        var newR = Array(repeating:RingEntry(), count:6)
+        newR = CopyRing(inputRing:inputRing)
+        for _ in (1...advancement){
+            newR = AdvanceRing(inputRing:newR)
+        }
+        return newR
+        
+    }
+    
+    
+    func RetractFrom(advancement:Int) -> Array<RingEntry>{
+        var Ring = Array(repeating:RingEntry(), count:6)
+        while RingAdvancement != advancement{
+            RingTransPersp = RetractRing(inputRing: RingTransPersp)
+            RingAdvancement = RingAdvancement - 1
+            if (RingAdvancement == -1){
+                RingAdvancement = 6
+            }
+        }
+        return Ring
+    }
+    
     
 }
+
+
+public class RotateableHouse{
+    public var Houses = Array(repeating:RotateableRing(), count:12)
+    public var HousesTransPersp = Array(repeating:RotateableRing(), count:12)
+    
+    func CopyHouses(inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH[0] = inputHouses[0]
+        newH[1] = inputHouses[1]
+        newH[2] = inputHouses[2]
+        newH[3] = inputHouses[3]
+        newH[4] = inputHouses[4]
+        newH[5] = inputHouses[5]
+        newH[6] = inputHouses[6]
+        newH[7] = inputHouses[7]
+        newH[8] = inputHouses[8]
+        newH[9] = inputHouses[9]
+        newH[10] = inputHouses[10]
+        newH[11] = inputHouses[11]
+        
+        return newH
+    }
+    
+    func AdvanceHouse(inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH[0] = inputHouses[11]
+        newH[1] = inputHouses[0]
+        newH[2] = inputHouses[1]
+        newH[3] = inputHouses[2]
+        newH[4] = inputHouses[3]
+        newH[5] = inputHouses[4]
+        newH[6] = inputHouses[5]
+        newH[7] = inputHouses[6]
+        newH[8] = inputHouses[7]
+        newH[9] = inputHouses[8]
+        newH[10] = inputHouses[9]
+        newH[11] = inputHouses[10]
+        
+        return newH
+    }
+    
+    func AdvanceNode(CelestialBody:String, House:Int, Ring:Int, inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH = CopyHouses(inputHouses:inputHouses)
+        newH[House].Ring = newH[House].RemoveAt(ringNumber: Ring, inputRing:newH[House].Ring)
+        newH[House].RingTransPersp = newH[House].RemoveAt(ringNumber: Ring, inputRing: newH[House].RingTransPersp)
+        
+        
+        if (House == 11){
+            newH[0].RingTransPersp = newH[0].InsertAt(inputRing: newH[0].Ring, CelestialBody: CelestialBody)
+        }
+        else {
+            newH[House + 1].RingTransPersp = newH[House + 1].InsertAt(inputRing: newH[House + 1].Ring, CelestialBody: CelestialBody)
+        }
+        
+        return newH
+    }
+    
+    func RetractNode(CelestialBody:String, House:Int, Ring:Int, inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH = CopyHouses(inputHouses:inputHouses)
+        newH[House].Ring = newH[House].RemoveAt(ringNumber: Ring, inputRing:newH[House].Ring)
+        newH[House].RingTransPersp = newH[House].RemoveAt(ringNumber: Ring, inputRing: newH[House].RingTransPersp)
+        
+        if (House == 0){
+            newH[11].RingTransPersp = newH[11].InsertAt(inputRing: newH[11].Ring, CelestialBody: CelestialBody)
+        }
+        else {
+            newH[House - 1].RingTransPersp = newH[House - 1].InsertAt(inputRing: newH[House - 1].Ring, CelestialBody: CelestialBody)
+        }
+        return newH
+        
+    }
+    
+    func AdvanceNodeForCycle(CelestialBody:String, House:Int, Ring:Int, Cycle:Int, inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH = CopyHouses(inputHouses:inputHouses)
+        for _ in (1...Cycle){
+            newH = AdvanceNode(CelestialBody:CelestialBody, House: House, Ring:Ring, inputHouses:newH)
+        }
+        return newH
+    }
+    
+    func RetractHouses(inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH[0] = inputHouses[1]
+        newH[1] = inputHouses[2]
+        newH[2] = inputHouses[3]
+        newH[3] = inputHouses[4]
+        newH[4] = inputHouses[5]
+        newH[5] = inputHouses[6]
+        newH[6] = inputHouses[7]
+        newH[7] = inputHouses[8]
+        newH[8] = inputHouses[9]
+        newH[9] = inputHouses[10]
+        newH[10] = inputHouses[11]
+        newH[11] = inputHouses[0]
+        
+        return newH
+    }
+    
+    func AdvanceTo(aHouseAdvancement:Int, inputHouses:Array<RotateableRing>) -> Array<RotateableRing>{
+        var newH = Array(repeating:RotateableRing(), count:12)
+        newH = CopyHouses(inputHouses:inputHouses)
+        for _ in (1...aHouseAdvancement){
+            newH = AdvanceHouse(inputHouses:newH)
+        }
+        return newH
+    }
+}
+
+public class AstrologicalProfile{
+    public var HouseInfo = RotateableHouse()
+    public var advancement = 1
+    public var cycle  = 1
+    public var RingAdvancement = 1
+    public var totalAdvancementIndicator = 1
+    public var Notes = Array(repeating:"", count:144)
+    public var IndividualName = ""
+    public var Category = ""
+    
+    
+    
+    func createUnfilledBlank() -> AstrologicalProfile{
+        var AP = AstrologicalProfile()
+        for i in (0...11){
+            var R = RotateableRing()
+            for j in (0...5){
+                var RE = RingEntry()
+                RE.CurrentCelestialBody =  ""
+                RE.originalIndicator = ""
+                R.Ring.append(RE)
+                R.RingTransPersp.append(RE)
+            }
+            AP.HouseInfo.Houses.append(R)
+            AP.HouseInfo.HousesTransPersp.append(R)
+        }
+      return AP
+    }
+
+}
+
+public class AstrologicalCategory{
+    public var Contents = Array<AstrologicalProfile>()
+}
+
+// Dictionary containing arrays of AP divided by Category
+public class AstrologicalDatabase{
+    public var Database = [String: AstrologicalCategory]()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ImageApp/ViewController.swift
+++ ImageApp/ViewController.swift
@@ -16,16 +16,23 @@ class ViewController: UIViewController, UIScrollViewDelegate{
     @IBOutlet weak var aUiView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imgPhoto: UIImageView!
-    let DM = DataModel()
+    let m_ADB = AstrologicalDatabase()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.delegate = self
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 6.0
-        self.DM.aTestString = "this"
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let AP = AstrologicalProfile()
+        AP.IndividualName = "Watashi"
+        
+        let AC = AstrologicalCategory()
+        AC.Contents.append(AP)
+        
+        m_ADB.Database["Individuals"] = AC
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
